8a3f24a0e4815c34700f100032c471533d6bcb81
sivachaitanya37 <sivachaitanya37@gmail.com>
2021-09-10 19:43:10 +0530

    Part11

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,17 +26,32 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+                                ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("M1 Incoming request");
+                await next();
+                logger.LogInformation("M1 Outgoing response");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("M2 Incoming request");
+                await next();
+                logger.LogInformation("M2 Outgoing response");
+            });
+
             app.Run(async (context) =>
             {
-                await context.Response
-                .WriteAsync(_config["MyKey"]);
+                await context.Response.WriteAsync("M3 Request handled and response produced");
+                logger.LogInformation("M3 Request handled and response produced");
             });
         }
     }
