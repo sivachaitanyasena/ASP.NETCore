9e99a106d2594b6595ba5a06f92e1a247e9cd82b
sivachaitanya37 <sivachaitanya37@gmail.com>
2021-09-10 21:16:56 +0530

    Part16-Setup mvc in asp net core

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from MVC";
+        }
+    }
+    //public class HomeController : Controller
+    //{
+    //    public IActionResult Index()
+    //    {
+    //        return View();
+    //    }
+    //}
+}
