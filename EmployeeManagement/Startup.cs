271eea3cd2cee7ebf388f452a5e96053389ba2ce
sivachaitanya37 <sivachaitanya37@gmail.com>
2021-09-10 18:39:35 +0530

    Part9

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,12 @@ namespace EmployeeManagement
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,7 +35,7 @@ namespace EmployeeManagement
             app.Run(async (context) =>
             {
                 await context.Response
-                .WriteAsync($"The current process used {System.Diagnostics.Process.GetCurrentProcess().ProcessName}");
+                .WriteAsync(_config["MyKey"]);
             });
         }
     }
